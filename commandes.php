a25f85abb46f1bf5bb6ccfbb29cd8535a049bace
MadelineTargaryen <45229127+MadelineTargaryen@users.noreply.github.com>
2019-01-23 10:41:47 +0100

    Add files via upload
    Coucou !

---
+++ commandes.php
@@ -0,0 +1,24 @@
+<?php
+
+class commandes {
+    
+    public $ID_commande;
+    public $ID_client;
+    public $ID_article;
+    public $date_commande;
+    public $prix_commande;
+    private $numCarte;
+    private $dateExpi;
+    private $crypto;
+    
+}
+
+//constructeur
+private function classCommandes() {
+    
+}
+
+
+
+
+?>
