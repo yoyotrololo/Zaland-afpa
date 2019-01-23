6d0c34684593f65e73e06831d04eb061a2fc18c0
Jonathan <44607958+jdsd45@users.noreply.github.com>
2019-01-23 15:35:54 +0100

    Create gestion_gestion.php

---
+++ gestion_gestion.php
@@ -0,0 +1,53 @@
+<?php
+
+try 
+{
+	$bdd = new PDO('mysql:host=172.14.140.8:3306;dbname=zalandafpa;charset=utf8','jonathan','123', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)	//permet de capturer l'exception en cas d'erreur, et d'éviter que le mot de passe ne s'affiche
+{
+	die('Erreur : ' . $e->getMessage());
+} 
+
+
+// ROUTEUR 
+
+$routeur = "delete";
+$id_client = 8;
+$id_client = $SESSION['id_client'];
+
+switch ($routeur) {
+	case "update":
+		// MODIFIER LES INFOS DU CLIENT 
+
+		// reception de tous les champs à modifier et de leurs nouvelles valeurs en JSON
+		$json ='[{"champModif": ["genre","nom"],"nouvellesValeurs": ["HOMME","targaryen"]}]';
+		$tab_modif = json_decode($json);
+		$taille_tab_modif = sizeof($tab_modif[0]->champModif);
+
+		// màj de tous les champs à modifier
+		for ($i=0 ; $i<$taille_tab_modif ; $i++) {
+			$colonne = $tab_modif[0]->champModif[$i];
+			$nouvelleValeur = $tab_modif[0]->nouvellesValeurs[$i];
+
+			$requete = "
+				UPDATE clients 
+				SET " . $tab_modif[0]->champModif[$i] . "= '" .  $tab_modif[0]->nouvellesValeurs[$i] . "' " . "
+				WHERE id_client = " . $id_client . "";
+			$bdd->exec($requete);
+		}
+		echo('Le compte du client n°' . $id_client . ' est mis à jour');		
+		break;
+
+	case "delete":
+		// SUPPRESSION DU COMPTE CLIENT
+		$requete = "
+			DELETE FROM clients 
+			WHERE id_client = " . $id_client . "";
+		$bdd->exec($requete);
+		echo('Le compte du client n°' . $id_client . ' est supprimé');
+		break;
+}
+
+
+?>
