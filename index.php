ff72a45e58df046f6762a41e8faee12f398bbfcb
Aegidios <45229186+Aegidios@users.noreply.github.com>
2019-01-23 17:00:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html">
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./bootstrap-4.2.1-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./bootstrap-social-gh-pages/assets/css/font-awesome.css" />
+    <link rel="stylesheet" href="./bootstrap-4.2.1-dist/css/bootstrap-grid.min.css" />
+    <link rel="stylesheet" href="./bootstrap-4.2.1-dist/css/bootstrap-reboot.min.css" />
+    <link rel="stylesheet" href="./bootstrap-social-gh-pages/bootstrap-social.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="accueil.css">
+    <script type="text/javascript" src="./jquery/jquery-3.3.1.min.js"></script>
+
+    <title>Zaland'AFPA | Site de vêtements en ligne</title>
+</head>
+
+<body>
+
+    <?php
+    
+    include('./menu.php');
+      
+    include('./navigation.php');
+      
+      
+      
+      
+      
+      
+      
+      include('./footer.php');
+      
+      ?>
+
+</body>
+
+</html>
--- navigation.php
+++ navigation.php
@@ -1,19 +1,12 @@
-<?php if (isset($_GET['x'])) {
-   if (strpos($_GET['x'], "/")) {
-      $dir = substr(str_replace('..', '', $_GET['x']), 0, strpos($_GET['x'], "/")) . "/";
-      $file = substr(strrchr($_GET['x'], "/"), 1);
-      if (file_exists($dir.$file.".php")) {
-         include($dir.$file.".php");
-      } else {
-         include("index2.php");
-      }
-   } else {
-      if (file_exists(basename($_GET['x']).".php")) {
-         include(basename($_GET['x']).".php");
-      } else {
-         include("index2.php");
-      }
-   }
-} else {
-   include("index2.php");
-} ?>
+<?php
+
+switch($page){
+    case "homme": include('./homme.php');
+        break;
+    case "femme": include('./femme.php');
+        break;
+    default: include('accueil.php');
+        break;
+}
+
+?>
