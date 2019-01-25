88ee54a7574910fe43b428dc430ef46e0a9204a4
Justin4949 <45229247+Justin4949@users.noreply.github.com>
2019-01-25 09:37:19 +0100

    menu

--- vue_menu.php
+++ vue_menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><i class="fa fa-home" style="font-size:35px;"></i></a>
+    <a class="navbar-brand" href="?page=accueil"><i class="fa fa-home" style="font-size:35px;"></i></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -7,10 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" style="font-size:1.5em; color:white;" href="#">Homme | <span class="sr-only">(current)</span></a>
+                <a class="nav-link" style="font-size:1.5em; color:white;" href="?page=homme">Homme | <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" style="font-size:1.5em; color: white;" href="#">Femme</a>
+                <a class="nav-link" style="font-size:1.5em; color: white;" href="?page=femme">Femme</a>
             </li>
         </ul>
         <nav class="navbar navbar-light bg-dark">
@@ -22,3 +22,12 @@
     </div>
 </nav>
 
+<?php
+
+if(isset($_GET['page'])){
+   $page = $_GET['page'];
+}else{
+   $page = "";
+}
+
+?>
