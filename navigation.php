80d4c5ccbb2a9fcc9f6194a2c46cb4237d69e8a1
Justin4949 <45229247+Justin4949@users.noreply.github.com>
2019-01-25 09:50:06 +0100

    menu + article

--- navigation.php
+++ navigation.php
@@ -1,9 +1,9 @@
 <?php
 
 switch($page){
-    case "article": include('./vue_article.php');
-        break;
     case "accueil": include('./vue_accueil.php');
+        break;
+    case "article": include('./vue_article.php');
         break;
     case "filter": include('./vue_filter.php');
         break;
