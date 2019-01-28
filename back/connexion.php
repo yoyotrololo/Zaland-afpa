6c338f2fd21a1d4cc79aebc1290487bd646d7d43
MadelineTargaryen <madeline.plard@gmail.com>
2019-01-28 10:18:22 +0100

    Update connexion.php

--- back/connexion.php
+++ back/connexion.php
@@ -3,7 +3,7 @@
  try
 {
     //connexion avec la BDD
-	$bdd = new PDO('mysql:host=localhost;dbname=zalandafpa;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=172.14.140.8;dbname=zalandafpa;charset=utf8', 'madeline', 'madeline');
 }
 catch (Exception $e)
 {
@@ -16,7 +16,7 @@ if (isset($_POST['mail']) && isset($_POST['pass'])) {
     $pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
 
-$req = $bdd->prepare('SELECT id_client, pass FROM clients WHERE mail = :mail');
+$req = $bdd->prepare('SELECT ID_client, pass FROM clients WHERE mail = :mail');
 $req->execute(array(
     'mail' => $mail));
     $resultat = $req->fetch();
@@ -32,7 +32,7 @@ else
 {
     if ($isPasswordCorrect) {
         session_start();
-        $_SESSION['id_client'] = $resultat['id_client'];
+        $_SESSION['ID_client'] = $resultat['ID_client'];
         $_SESSION['mail'] = $mail;
         echo 'Vous êtes connecté !';
         $_SESSION['connexion'] = true;
@@ -45,6 +45,6 @@ else
 
 }
 
-//lien header location
+header('Location: index.php');
 
 ?>
