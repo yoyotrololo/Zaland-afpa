6197dfd60779e3fe566b3ad20d057bad8608968b
MadelineTargaryen <madeline.plard@gmail.com>
2019-01-28 15:56:40 +0100

    MAJ articles & filtres

--- back/filtres.php
+++ back/filtres.php
@@ -5,7 +5,7 @@ require_once('./class/articles.php');
 try
 {
     //connexion avec la BDD
-	$bdd = new PDO('mysql:host=172.14.140.8;dbname=zalandafpa;charset=utf8', 'madeline', 'madeline');
+	$bdd = new PDO('mysql:host=172.14.140.8;dbname=zalandafpa;charset=utf8', DB_USER, DB_PASSWORD);
 }
 catch (Exception $e)
 {
@@ -18,10 +18,8 @@ while ($donnees = $req->fetch())
 
 {
     $article = new articles($donnees['ID_article'], $donnees['nom'], $donnees['genre'], $donnees['type'], $donnees['couleur'], $donnees['taille'], $donnees['prix'], $donnees['lien_image']);
-    var_dump($article);
     $tab_article[] = $article; 
-    
-    
+
     echo json_encode($tab_article);
 }
 ?>
