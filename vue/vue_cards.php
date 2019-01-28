969027c52604838bc7a4364eaa5486c144a6ce40
Justin4949 <45229247+Justin4949@users.noreply.github.com>
2019-01-28 13:22:02 +0100

    MAJ Accueil

--- vue/vue_cards.php
+++ vue/vue_cards.php
@@ -19,7 +19,7 @@ catch (Exception $e)
   <div class="card">
     <img src="./img/chemise_bleue.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Chemise Bleue Homme</h5>
+      <h5 class="card-title">Chemise Bleue - Homme</h5>
       <p class="card-text">Description produit</p>
         <a href="#" class="btn btn-primary">Voir article</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
@@ -29,9 +29,9 @@ catch (Exception $e)
         
         <div class="col-sm-4">
   <div class="card">
-    <img src="./img/chemise_jean.jpg" class="card-img-top" alt="...">
+    <img src="./img/chemise_jeans.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Chemise en Jeans</h5>
+      <h5 class="card-title">Chemise Jeans - Homme</h5>
       <p class="card-text">Description produit</p>
         <a href="#" class="btn btn-primary">Voir article</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
@@ -42,7 +42,7 @@ catch (Exception $e)
   <div class="card">
     <img src="./img/pantalon.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Pantalon</h5>
+      <h5 class="card-title">Pantalon - Homme</h5>
       <p class="card-text">Description produit</p>
         <a href="#" class="btn btn-primary">Voir article</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
