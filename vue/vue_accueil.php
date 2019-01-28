70da1716c1a76d22abd59543ab96bddfb391a279
Justin4949 <45229247+Justin4949@users.noreply.github.com>
2019-01-28 16:36:54 +0100

    MAJ Accueil

--- vue/vue_accueil.php
+++ vue/vue_accueil.php
@@ -16,52 +16,107 @@
 </div>  -->
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <div class="row">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
 
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active" class="col-sm-2">
-      <img src="./img/chemise_bleue.jpg" class="d-block w-25" alt="...">
-        
-    </div>
-    <div class="carousel-item" class="col-sm-2">
-      <img src="./img/chemise_jean.jpg" class="d-block w-25" alt="...">
+        <div class="carousel-item active ">
+            <img src="./img/veste_grise.jpg" class="d-block h-100 " alt="...">
+        </div>
 
-        
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active" class="col-sm-2">
-      <img src="./img/chemise_bleue.jpg" class="d-block w-25" alt="...">
-        
-    </div>
-    <div class="carousel-item" class="col-sm-2">
-      <img src="./img/chemise_jeans.jpg" class="d-block w-25" alt="...">
+        <div class="carousel-item ">
+            <img src="./img/manteau_homme.jpg" class="d-block h-100" alt="...">
+        </div>
 
+        <div class="carousel-item ">
+            <img src="./img/chaussures_nike.jpg" class="d-block h-100" alt="...">
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-    <div class="carousel-item" class="col-sm-2">
-      <img src="./img/pantalon.jpg" class="d-block w-25" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-    </div>
-
 
 <?php
 include ('./vue/vue_cards.php');
 ?>
+
+<div class="container-fluid" style="padding-right: 0px; padding-left:0px;">
+
+    <h2>Notre sélection Femme</h2>
+</div>
+
+
+    <div class="card-group">
+
+        <div class="card">
+            <img src="./img/jupe.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Jupe Noire - Femme</h5>
+                <p class="card-text">Description produit</p>
+                <a href="#" class="btn btn-primary">Voir article</a>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./img/haut_vert.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Haut vert - Femme</h5>
+                <p class="card-text">Description produit</p>
+                <a href="#" class="btn btn-primary">Voir article</a>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./img/manteau.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Manteau Gris - Femme</h5>
+                <p class="card-text">Description produit</p>
+                <a href="#" class="btn btn-primary">Voir article</a>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+
+        <div class="carousel-item active ">
+            <img src="./img/bottes_leopard.jpg" class="d-block h-100 " alt="...">
+        </div>
+
+        <div class="carousel-item ">
+            <img src="./img/chaussures_noires.jpg" class="d-block h-100" alt="...">
+        </div>
+
+        <div class="carousel-item ">
+            <img src="./img/sac.jpg" class="d-block h-100" alt="...">
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
