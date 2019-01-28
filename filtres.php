243d6d808568b9088bf107957298483190f99235
MadelineTargaryen <madeline.plard@gmail.com>
2019-01-28 09:22:23 +0100

    Create filtres.php

---
+++ filtres.php
@@ -0,0 +1,26 @@
+<?php
+require_once('articles.php');
+
+try
+{
+    //connexion avec la BDD
+	$bdd = new PDO('mysql:host=172.14.140.8;dbname=zalandafpa;charset=utf8', 'madeline', 'madeline');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+$req = $bdd->query('SELECT * FROM articles WHERE genre = "Femme"');
+
+while ($donnees = $req->fetch())
+
+{
+    $article = new articles($donnees['ID_article'], $donnees['nom'], $donnees['genre'], $donnees['type'], $donnees['couleur'], $donnees['taille'], $donnees['prix'], $donnees['lien_image']);
+    var_dump($article);
+    $tab_article[] = $article; 
+    
+    
+    echo json_encode($tab_article);
+}
+?>
