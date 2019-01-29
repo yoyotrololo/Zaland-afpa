d6b65e2897d2dc376595f2b5053afbad81dccc6e
Monnetkevin <45229238+Monnetkevin@users.noreply.github.com>
2019-01-29 14:15:12 +0100

    Update vue_filter.php

--- vue/vue_filter.php
+++ vue/vue_filter.php
@@ -1,125 +1,93 @@
 
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
-	<link rel="stylesheet" href="./css/reset.css"> <!-- CSS reset -->
-	<link rel="stylesheet" href="./css/style.css"> <!-- Resource style -->
-	<script src="./js/modernizr.js"></script> <!-- Modernizr -->
-  	
+    <link rel="stylesheet" href="./css/reset.css"> <!-- CSS reset -->
+    <link rel="stylesheet" href="./css/style.css"> <!-- Resource style -->
+
+
 </head>
+
 <body>
 
 
-	<main class="cd-main-content">
-		<div class="cd-tab-filter-wrapper">
-			<div class="cd-tab-filter">
-				<ul class="cd-filters">
-					<li class="placeholder"> 
-						<a data-type="all" href="#0">Tous les articles</a> <!-- selected option on mobile -->
-					</li> 
-					<li class="filter"><a class="selected" href="#0" data-type="all">Tous les articles</a></li>
-					<li class="filter" data-filter=".color-1"><a href="#0" data-type="color-1">Homme</a></li>
-					<li class="filter" data-filter=".color-2"><a href="#0" data-type="color-2">Femme</a></li>
-				</ul> <!-- cd-filters -->
-			</div> <!-- cd-tab-filter -->
-		</div> <!-- cd-tab-filter-wrapper -->
+    <main class="cd-main-content">
+        <div class="cd-tab-filter-wrapper">
+            <div class="cd-tab-filter">
+                <ul class="cd-filters">
+                    <li class="filter"><a class="btn_genre" id="All" name="All" href="#All" data-type="all">Tous les articles</a></li>
+                    <li class="filter" data-filter=".color-1"><a name="Femme" id="Femme" class="btn_genre" href="#Femme" data-type="color-1">Femme</a></li>
+                    <li class="filter" data-filter=".color-2"><a name="Homme" id="Homme" class="btn_genre" href="#Homme" data-type="color-2">Homme</a></li>
+                </ul> <!-- cd-filters -->
 
-		<section class="cd-gallery">
-			<ul>
-				<li class="mix color-1 check1 radio2 option3"><img src="img/img-1.jpg" alt="Image 1"></li>
-				<li class="mix color-2 check2 radio2 option2"><img src="img/img-2.jpg" alt="Image 2"></li>
-				<li class="mix color-1 check3 radio3 option1"><img src="img/img-3.jpg" alt="Image 3"></li>
-				<li class="mix color-1 check3 radio2 option4"><img src="img/img-4.jpg" alt="Image 4"></li>
-				<li class="mix color-1 check1 radio3 option2"><img src="img/img-5.jpg" alt="Image 5"></li>
-				<li class="mix color-2 check2 radio3 option3"><img src="img/img-6.jpg" alt="Image 6"></li>
-				<li class="mix color-2 check2 radio2 option1"><img src="img/img-7.jpg" alt="Image 7"></li>
-				<li class="mix color-1 check1 radio3 option4"><img src="img/img-8.jpg" alt="Image 8"></li>
-				<li class="mix color-2 check1 radio2 option3"><img src="img/img-9.jpg" alt="Image 9"></li>
-				<li class="mix color-1 check3 radio2 option4"><img src="img/img-10.jpg" alt="Image 10"></li>
-				<li class="mix color-1 check3 radio3 option2"><img src="img/img-11.jpg" alt="Image 11"></li>
-				<li class="mix color-2 check1 radio3 option1"><img src="img/img-12.jpg" alt="Image 12"></li>
-				<li class="gap"></li>
-				<li class="gap"></li>
-				<li class="gap"></li>
-			</ul>
-			<div class="cd-fail-message">Aucun résultats</div>
-		</section> <!-- cd-gallery -->
+            </div> <!-- cd-tab-filter -->
+        </div>
+        <div class="cd-gallery">
+            <ul id="articles">
+                <!--<li class="mix color-1 check1 radio2 option3"><img src="img/img-1.jpg" alt="Image 1"></li>-->
+            </ul>
+        </div><!-- cd-tab-filter-wrapper -->
 
-		<div class="cd-filter">
-			<form>
-				<div class="cd-filter-block">
-					<h4>Rechercher</h4>
-					
-					<div class="cd-filter-content">
-						<input type="search" placeholder="Try color-1...">
-					</div> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
-
-				<div class="cd-filter-block">
-					<h4>Check boxes</h4>
-
-					<ul class="cd-filter-content cd-filters list">
-						<li>
-							<input class="filter" data-filter=".check1" type="checkbox" id="checkbox1">
-			    			<label class="checkbox-label" for="checkbox1">Haut</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".check2" type="checkbox" id="checkbox2">
-							<label class="checkbox-label" for="checkbox2">Option 2</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".check3" type="checkbox" id="checkbox3">
-							<label class="checkbox-label" for="checkbox3">Option 3</label>
-						</li>
-					</ul> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
-
-				<div class="cd-filter-block">
-					<h4>Select</h4>
-					
-					<div class="cd-filter-content">
-						<div class="cd-select cd-filters">
-							<select class="filter" name="selectThis" id="selectThis">
-								<option value="">Choose an option</option>
-								<option value=".option1">Option 1</option>
-								<option value=".option2">Option 2</option>
-								<option value=".option3">Option 3</option>
-								<option value=".option4">Option 4</option>
-							</select>
-						</div> <!-- cd-select -->
-					</div> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
-
-				<div class="cd-filter-block">
-					<h4>Radio buttons</h4>
-
-					<ul class="cd-filter-content cd-filters list">
-						<li>
-							<input class="filter" data-filter="" type="radio" name="radioButton" id="radio1" checked>
-							<label class="radio-label" for="radio1">All</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".radio2" type="radio" name="radioButton" id="radio2">
-							<label class="radio-label" for="radio2">Choice 2</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".radio3" type="radio" name="radioButton" id="radio3">
-							<label class="radio-label" for="radio3">Choice 3</label>
-						</li>
-					</ul> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
-			</form>
-
-			<a href="#0" class="cd-close">Fermer</a>
-		</div> <!-- cd-filter -->
-
-		<a href="#0" class="cd-filter-trigger">Filtres</a>
-	</main> <!-- cd-main-content -->
-<script src="js/jquery-2.1.1.js"></script>
-<script src="js/jquery.mixitup.min.js"></script>
-<script src="js/main.js"></script> <!-- Resource jQuery -->
+    </main>
 </body>
+
+<script>
+    $(document).ready(function() {
+                var requete = $.get("http://localhost/zaland-afpa/back/filtres.php")
+                    .always(function() {
+                        var json = JSON.parse(requete.responseText);
+                        //console.log(json);
+                        $(json).each(function(i) {
+                            var article = json[i];
+                            var li_article = document.createElement('li');
+                            var balise_img = document.createElement('img');
+                            var url_img = "./img/" + article.lien_image;
+                            balise_img.setAttribute('src', url_img);
+                            li_article.setAttribute('class', 'mix color-1 check1 radio2 option3');
+                            $(li_article).append(balise_img);
+                            $('#articles').append(li_article);
+                        })
+                    })
+
+
+
+                $(".btn_genre").click(function() {
+                        //            var target = $(event.target);
+                        //            
+                        //			$('.cd-tab-filter .selected').removeClass('selected');
+                        //			target.addClass('selected');
+                        //            
+
+                        console.log($(this));
+                        //$(this).addClass('selected');
+                        $('#articles').empty();
+                        var genre = this.id;
+                        var requete = $.get("http://localhost/zaland-afpa/back/filtres.php", {
+                                genre: genre
+                            })
+                            .always(function() {
+                                    var json = JSON.parse(requete.responseText);
+                                    //console.log(json);
+                                    $(json).each(function(i) {
+                                            var article = json[i];
+                                            var li_article = document.createElement('li');
+                                            var balise_img = document.createElement('img');
+                                            var url_img = "./img/" + article.lien_image;
+                                            balise_img.setAttribute('src', url_img);
+                                            li_article.setAttribute('class', 'mix color-1 check1 radio2 option3');
+                                            $(li_article).append(balise_img);
+                                            $('#articles').append(li_article);
+                                            var nom = article.nom;
+                                            var prix = article.prix;
+                                            $(li_article).hover(function() {
+                                                   $(this).append(nom);
+                                                   $(this).append(prix);
+                                              
+                                                })
+                                         
+                                            })
+                                    })
+                            })
+                })
+
+</script>
