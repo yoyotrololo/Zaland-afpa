0d8949c7cf53eb0cf447b67ace9fa608a3e4ed09
Justin4949 <45229247+Justin4949@users.noreply.github.com>
2019-01-29 15:28:42 +0100

    MAJ Header/Footer

--- vue/vue_accueil.php
+++ vue/vue_accueil.php
@@ -1,20 +1,3 @@
-<!-- <div class="container-fluid" id="slide">
-    <div id="block">
-        <div class="row">
-            <div class="col-sm-4"></div>
-                <div class="col-sm-4" id='slider'>
-                    <figure>
-                        <img src="img/homme1.jpg" alt="Homme1">
-                        <img src="img/homme2.jpg" alt="Homme2">
-                        <img src="img/homme3.jpg" alt="Homme3">
-                        <img src="img/femme1.jpg" alt="Femme1">
-                        <img src="img/femme2.jpg" alt="Femme2">
-                    </figure>
-                </div>
-        </div>
-    </div>
-</div>  -->
-
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
--- vue/vue_footer.php
+++ vue/vue_footer.php
@@ -4,120 +4,120 @@
     <!-- Footer Links -->
     <div class="container text-center text-md-left">
 
-      <!-- Grid row -->
-      <div class="row">
-<hr class="clearfix w-100 d-md-none">
+        <!-- Grid row -->
+        <div class="row">
+            <hr class="clearfix w-100 d-md-none">
 
-        <!-- Grid column -->
-        <div class="col-md-5 mx-auto">
+            <!-- Grid column -->
+            <div class="col-md-5 mx-auto">
 
-          <!-- Links -->
-          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Aide</h5>
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Aide</h5>
 
-          <ul class="list-unstyled">
-            <li>
-              <a href="#!">Questions fréquentes</a>
-            </li>
-            <li>
-              <a href="#!">Essayer d'abord, payez après!</a>
-            </li>
-            <li>
-              <a href="#!">Livraison 24h gratuite en point retrait</a>
-            </li>
-                <li>
-              <a href="#!">Retour depuis votre boîte aux lettres</a>
-            </li>
-                  <li>
-              <a href="#!">Suivre mon colis</a>
-            </li>
-                     <li>
-              <a href="#!">Moyens de paiement</a>
-            </li>
-                     <li>
-              <a href="#!">Guides des tailles</a>
-            </li>
-                     <li>
-              <a href="#!">Retour & remboursement</a>
-            </li>
-                     <li>
-              <a href="#!">10% de réduction avec la newletter</a>
-            </li>
-              
-          </ul>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Questions fréquentes</a>
+                    </li>
+                    <li>
+                        <a href="#!">Essayer d'abord, payez après!</a>
+                    </li>
+                    <li>
+                        <a href="#!">Livraison 24h gratuite en point retrait</a>
+                    </li>
+                    <li>
+                        <a href="#!">Retour depuis votre boîte aux lettres</a>
+                    </li>
+                    <li>
+                        <a href="#!">Suivre mon colis</a>
+                    </li>
+                    <li>
+                        <a href="#!">Moyens de paiement</a>
+                    </li>
+                    <li>
+                        <a href="#!">Guides des tailles</a>
+                    </li>
+                    <li>
+                        <a href="#!">Retour & remboursement</a>
+                    </li>
+                    <li>
+                        <a href="#!">10% de réduction avec la newletter</a>
+                    </li>
+
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-4 mx-auto">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Chèques cadeaux</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Offrir une carte cadeau</a>
+                    </li>
+                    <li>
+                        <a href="#!">Utiliser une carte cadeau</a>
+                    </li>
+                    <li>
+                        <a href="#!">Chèques cadeaux d'entreprise</a>
+                    </li>
+
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-3 mx-auto">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Zaland'AFPA</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Corporate Website</a>
+                    </li>
+                    <li>
+                        <a href="#!">Emplois</a>
+                    </li>
+                    <li>
+                        <a href="#!">Presse</a>
+                    </li>
+                    <li>
+                        <a href="#!">Programme d'affiliation</a>
+                    </li>
+                    <li>
+                        <a href="#!">Zaland'AFPA Marketing Services</a>
+                    </li>
+                    <li>
+                        <a href="#!">Questions marketing</a>
+                    </li>
+                    <li>
+                        <a href="#!">Programme partenaire</a>
+                    </li>
+                    <li>
+                        <a href="#!">Lexique de la mode</a>
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+
+            <!-- Grid column -->
 
         </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-4 mx-auto">
-
-          <!-- Links -->
-          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Chèques cadeaux</h5>
-
-          <ul class="list-unstyled">
-            <li>
-              <a href="#!">Offrir une carte cadeau</a>
-            </li>
-            <li>
-              <a href="#!">Utiliser une carte cadeau</a>
-            </li>
-            <li>
-              <a href="#!">Chèques cadeaux d'entreprise</a>
-            </li>
-            
-          </ul>
-
-        </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-3 mx-auto">
-
-          <!-- Links -->
-          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Zaland'AFPA</h5>
-
-          <ul class="list-unstyled">
-            <li>
-              <a href="#!">Corporate Website</a>
-            </li>
-            <li>
-              <a href="#!">Emplois</a>
-            </li>
-            <li>
-              <a href="#!">Presse</a>
-            </li>
-            <li>
-              <a href="#!">Programme d'affiliation</a>
-            </li>
-                <li>
-              <a href="#!">Zaland'AFPA Marketing Services</a>
-            </li>
-                <li>
-              <a href="#!">Questions marketing</a>
-            </li>
-                <li>
-              <a href="#!">Programme partenaire</a>
-            </li>
-                 <li>
-              <a href="#!">Lexique de la mode</a>
-            </li>
-          </ul>
-
-        </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-      
-        <!-- Grid column -->
-
-      </div>
-      <!-- Grid row -->
+        <!-- Grid row -->
 
     </div>
     <!-- Footer Links -->
@@ -126,12 +126,21 @@
 
     <!-- Call to action -->
     <ul class="list-unstyled list-inline text-center py-2">
-      <li class="list-inline-item">
-        <h5 class="mb-1">Register for free</h5>
-      </li>
-      <li class="list-inline-item">
-        <a href="#!" class="btn btn-danger btn-rounded">Sign up!</a>
-      </li>
+        <li class="list-inline-item">
+            <a href="#!" style="text-decoration:none;">Mentions légales |</a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" style="text-decoration:none;">Protéction des données |</a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" style="text-decoration:none;">CGV |</a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" style="text-decoration:none;">*Conditions des offres |</a>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" style="text-decoration:none;">Traitement des données</a>
+        </li>
     </ul>
     <!-- Call to action -->
 
@@ -139,33 +148,33 @@
 
     <!-- Social buttons -->
     <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <i class="fa fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <i class="fa fa-twitter"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <i class="fa fa-google"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-          <i class="fa fa-linkedin"> </i>
-        </a>
-      </li>
-  
+        <li class="list-inline-item">
+            <a class="btn-floating btn-fb mx-1">
+                <i class="fa fa-facebook-f" style="font-size:35px;color:#3B5998;"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-tw mx-1">
+                <i class="fa fa-twitter" style="font-size:35px;color:#55ACEE;"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-gplus mx-1">
+                <i class="fa fa-google" style="font-size:35px;color:#dd4b39;"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-li mx-1">
+                <i class="fa fa-linkedin" style="font-size:35px;color:#007bb5;"> </i>
+            </a>
+        </li>
+
     </ul>
     <!-- Social buttons -->
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2019 Copyright:
-      <a href="#"> DWWM | Session 2018-2019</a>
+        <a href="#"> DWWM | Session 2018-2019</a>
     </div>
     <!-- Copyright -->
 
